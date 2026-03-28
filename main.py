104f1c15b86787320643cd287fa019fc7e54752b
Matvey Kaigorodcev <gehbect@gmail.com>
2026-03-29 00:41:27 +0500

    first get

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+tasks = []
+
+class STask(BaseModel):
+    id:str
+    title:str
+    completed:bool
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
