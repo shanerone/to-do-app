374e391e522481da281af06c1dab063bb823cffd
Matvey Kaigorodcev <gehbect@gmail.com>
2026-03-29 00:55:22 +0500

    post endpoint

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from fastapi import FastAPI
+from uuid import uuid4
+
 from pydantic import BaseModel
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-tasks = []
 
-class STask(BaseModel):
-    id:str
-    title:str
-    completed:bool
+class STasks(BaseModel):
+    id: str
+    title: str
+    completed: bool
+
+
+class STaskAdd(BaseModel):
+    title: str
+
+
+tasks: list[STasks] = []
+
 
 @app.get("/tasks")
-def get_tasks():
+async def get_tasks() -> list[STasks]:
     return tasks
+
+
+@app.post("/tasks")
+async def add_task(payload: STaskAdd) -> STasks:
+    task = STasks(id=str(uuid4()), title=payload.title, completed=False)
+
+    tasks.append(task)
+    return task
